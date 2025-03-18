a3f1056ae703a9af0552210ca31a4868c0a78bef
Rick Easley <rick.easley@gmail.com>
2025-03-18 19:18:28 -0400

    done up to exercise 4, still working through create_picture() method

--- backend/routes.py
+++ backend/routes.py
@@ -35,30 +35,47 @@ def count():
 ######################################################################
 @app.route("/picture", methods=["GET"])
 def get_pictures():
-    pass
+    if data:
+        return jsonify(data), 200
+    
+    return {"message": "Data not found"}, 404
 
 ######################################################################
 # GET A PICTURE
 ######################################################################
-
-
 @app.route("/picture/<int:id>", methods=["GET"])
 def get_picture_by_id(id):
-    pass
+    if data:
+        for picture in data:
+            if int(picture['id']) == id:
+                return jsonify(picture), 200
+    
+    return jsonify({"error": "ID not found"}), 404
 
 
 ######################################################################
 # CREATE A PICTURE
 ######################################################################
-@app.route("/picture", methods=["POST"])
+@app.route("/picture/<int:id>", methods=["POST"])
 def create_picture():
-    pass
+    if id in data:
+        return jsonify({"Message": f"picture with id {picture['id']} already exists"}), 302
+    
+    new_pic = request.get_json()
+    if not new_pic:
+        return jsonify({"error": "Invalid or missing JSON data"}), 400
+
+    try:
+        data.append(new_pic)
+    except NameError:
+        return {"error": "Data not defined"}, 500
+    
+    return {"message": f"{new_pic['id']} successfully created"}, 200
+
 
 ######################################################################
 # UPDATE A PICTURE
 ######################################################################
-
-
 @app.route("/picture/<int:id>", methods=["PUT"])
 def update_picture(id):
     pass
