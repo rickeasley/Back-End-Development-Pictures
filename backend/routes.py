bd2e430eff32a5aaf3c374edc064b60c51a1d974
Rick Easley <rick.easley@gmail.com>
2025-03-18 20:29:41 -0400

    finished exercise 6 with all unit tests passing

--- backend/routes.py
+++ backend/routes.py
@@ -35,9 +35,10 @@ def count():
 ######################################################################
 @app.route("/picture", methods=["GET"])
 def get_pictures():
+    # verifying there is data to search
     if data:
         return jsonify(data), 200
-    
+    # if no data, returning 404 status code
     return {"message": "Data not found"}, 404
 
 ######################################################################
@@ -45,32 +46,41 @@ def get_pictures():
 ######################################################################
 @app.route("/picture/<int:id>", methods=["GET"])
 def get_picture_by_id(id):
+    # making sure data is there to query
     if data:
+        # searching to see if the id is present in the data
         for picture in data:
             if int(picture['id']) == id:
                 return jsonify(picture), 200
-    
+    # id was not found, sending 404 status code
     return jsonify({"error": "ID not found"}), 404
 
 
 ######################################################################
 # CREATE A PICTURE
 ######################################################################
-@app.route("/picture/<int:id>", methods=["POST"])
+@app.route("/picture", methods=["POST"])
 def create_picture():
-    if id in data:
-        return jsonify({"Message": f"picture with id {picture['id']} already exists"}), 302
-    
-    new_pic = request.get_json()
-    if not new_pic:
-        return jsonify({"error": "Invalid or missing JSON data"}), 400
+    # getting the json data sent through POST
+    req_data = request.get_json()
+
+    # making sure data is present
+    if not req_data:
+        return jsonify({"error": "Missing required data"}), 400
+
+    # Check if ID already exists
+    for pic in data:
+        if pic['id'] == req_data['id']:
+            return jsonify({"Message": f"picture with id {req_data['id']} already present"}), 302
 
     try:
-        data.append(new_pic)
+        # Add to data dictionary
+        data.append(req_data)
     except NameError:
-        return {"error": "Data not defined"}, 500
+        return jsonify({"effor": "unexpected error"}), 500
     
-    return {"message": f"{new_pic['id']} successfully created"}, 200
+    #successfully added
+    return jsonify(req_data), 201
 
 
 ######################################################################
@@ -78,11 +88,29 @@ def create_picture():
 ######################################################################
 @app.route("/picture/<int:id>", methods=["PUT"])
 def update_picture(id):
-    pass
+    update_data = request.get_json()
+    if not update_data:
+        return jsonify({"error": "Invalid data sent."}), 400
+
+    for picture in data:
+        if picture['id'] == id:
+            picture.update(update_data)
+            return jsonify({"message": f"Picture wiht id {id} successfully updated"}), 200
+    
+    return jsonify({"message": "picture not found"}), 404
 
 ######################################################################
 # DELETE A PICTURE
 ######################################################################
 @app.route("/picture/<int:id>", methods=["DELETE"])
 def delete_picture(id):
-    pass
+    if not id:
+        return jsonify({"error": "Invalid input"}), 400
+
+    for picture in data:
+        if picture['id'] == id:
+            del data[id]
+            return '', 204
+
+    return jsonify({"message": "picture not found"}), 404
+
